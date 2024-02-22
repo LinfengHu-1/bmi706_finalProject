9a054bdb579d68a85bf0c18faa1f6d86b4f232b1
LinfengHu-1 <hlinfeng@yahoo.com>
2024-02-22 13:02:13 -0500

    change state code to name

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,39 +21,22 @@ tab1, tab2 = st.tabs(["Mental Health Outcomes", "Access to Psychiatric Care"])
 ################### Tab 1: Mental Health Outcomes #######################
 with tab1:
    task1 = st.header("Mental Health Outcomes: Temporal Trends")
-   
-
-year = st.slider("Year", min_value=df["Year"].min(), max_value=df["Year"].max())
-sex = st.radio("Sex", options = df["Sex"].unique())
-countries = st.multiselect("Countries", options=df["Country"].unique(), default=[
-    "Austria",
-    "Germany",
-    "Iceland",
-    "Spain",
-    "Sweden",
-    "Thailand",
-    "Turkey",
-])
-cancer = st.selectbox("Cancer", options = df["Cancer"].unique())
-ages = [
-    "Age <5",
-    "Age 5-14",
-    "Age 15-24",
-    "Age 25-34",
-    "Age 35-44",
-    "Age 45-54",
-    "Age 55-64",
-    "Age >64",
-]
-subset = df[(df["Year"] == year) & (df["Sex"] == sex) & (df["Country"].isin(countries)) & (df["Cancer"] == cancer)]
-chart = alt.Chart(subset).mark_rect().encode(
-    x=alt.X("Age", sort=ages),
-    y=alt.Y("Country", title="Country"),
-    color=alt.Color("Rate", title="Mortality Rate (log scale)", scale=alt.Scale(type='log', domain=[0.01, 1000], clamp=True)),
-    tooltip=["Rate"],
+   states = st.multiselect("States", options=df_stackedState["STATEFIP"].unique())
+   chart = alt.Chart(df_stackedState).mark_line().encode(
+    x=alt.X("Year", sort=YEAR),
+    y=alt.Y("MH1_values_1", title="MH1_values_1"),
+    #color=alt.Color("Rate", title="Mortality Rate (log scale)", scale=alt.Scale(type='log', domain=[0.01, 1000], clamp=True)),
+    #tooltip=["Rate"],
 ).properties(
-    title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+    title=f"Mental Health Disorder rates in {states}",
 )
+
+#year = st.slider("Year", min_value=df["Year"].min(), max_value=df["Year"].max())
+#sex = st.radio("Sex", options = df["Sex"].unique())
+#cancer = st.selectbox("Cancer", options = df["Cancer"].unique())
+#ages = ["Age <5", "Age 5-14",]
+#subset = df[(df["Year"] == year) & (df["Sex"] == sex) & (df["Country"].isin(countries)) & (df["Cancer"] == cancer)]
+
 
 ### P2.5 ###
 st.altair_chart(chart, use_container_width=True)
@@ -66,16 +49,7 @@ if len(countries_in_subset) != len(countries):
         st.write("No data available for " + ", ".join(missing) + ".")
 
 
-### BONUS ###
-chart2 = alt.Chart(subset).mark_bar().encode(
-    #x=alt.X("Age", sort=ages),
-    y=alt.Y("Country", title="Country"),
-    x=alt.Y("Pop", title="Population Size"),
-    tooltip=["Pop"],
-).properties(
-    title=f"Population size for {'males' if sex == 'M' else 'females'} in {year}",
-)
-st.altair_chart(chart2, use_container_width=True)
+
 
 
 
