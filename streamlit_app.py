5524d926b43b4a7ed6e7975c98768dfc7de55982
LinfengHu-1 <hlinfeng@yahoo.com>
2024-02-23 14:39:58 -0500

    try add vega_datasets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from vega_datasets import data
 
 from io import StringIO
 from io import BytesIO
@@ -8,34 +9,42 @@ from zipfile import ZipFile
 from urllib.request import urlopen
 
 ################# Loading & Layout ##########################
+st.set_page_config(layout="wide")
+
 @st.cache_data
 def load_data():
-   #df_mergedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/merged_data_state.csv")
    df_stackedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_state.csv")
    df_stackedDiag = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_diagnosis.csv")
    return df_stackedState, df_stackedDiag
 df_stackedState, df_stackedDiag = load_data()
 
-st.dataframe(df_stackedDiag.head())
-
-### General Layout ###
+### Layout ###
 st.write("## Mental Health Outcomes and Intervention Investigation")
 tab1, tab2 = st.tabs(["Mental Health Outcomes", "Access to Psychiatric Care"])
 
 ################### Tab 1: Mental Health Outcomes #######################
 with tab1:
-   task1 = st.header("Mental Health Outcomes: Temporal Trends")
-   diagnosis = st.multiselect("Mental Health Outcome", options=df_stackedDiag["MH1"].unique())
-   #filtered_df = df_stackedDiag[df_stackedDiag["STATEFIP"].isin(diagnosis)]
-   chart = alt.Chart(df_stackedDiag).mark_line().encode(
-      x="YEAR",
+    ### Tab 1, Task 1 ###
+   task1 = st.header("Temporal Trends")
+   df_stackedDiag.rename(columns={'MH1': 'Mental Health Outcomes'}, inplace=True)
+   df_stackedDiag = df_stackedDiag[df_stackedDiag['Mental Health Outcomes'] != 'Missing']
+   diagnosis = st.multiselect("Mental Health Outcome", options=df_stackedDiag["Mental Health Outcomes"].unique(), 
+                              default = ['Trauma/Stress-related Disorder', 'Anxiety Disorder', 'ADHD'])
+   filtered_df = df_stackedDiag[df_stackedDiag["Mental Health Outcomes"].isin(diagnosis)]
+   filtered_df['YEAR'] = filtered_df['YEAR'].astype(str)
+   chart = alt.Chart(filtered_df).mark_line().encode(
+      x=alt.X("YEAR", axis=alt.Axis(format='', title='Year', labelAngle=0)),
       y=alt.Y("Population", title="Total Number of Patients"),
-      color="MH1:N",  # Color by state if needed
+      color="Mental Health Outcomes:N",  # Color by state if needed
       tooltip=["YEAR", "Population"]
       ).properties(
-         title=f"Mental Health Disorder rates of {diagnosis}",
+         title=f"Mental Health Disorder rates of {', '.join(diagnosis)}",
          )
    st.altair_chart(chart, use_container_width=True)
+
+   ### Tab 1, Task 2 ###
+   task2 = st.header("Geospatial Pattern")
+
 
 
 
@@ -54,7 +63,7 @@ with tab1:
 
 ############## Tab 2: Mental Health Outcomes ##########################
 with tab2:
-   task1 = st.header("Access to Care: Temporal Trends")
+   task1 = st.header("Temporal Trends")
 
 
 
