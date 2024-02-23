b6862f916664407ab0f3a911b760cbdaad1dd399
LinfengHu-1 <hlinfeng@yahoo.com>
2024-02-23 12:26:22 -0500

    try use cache_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,28 +8,36 @@ from zipfile import ZipFile
 from urllib.request import urlopen
 
 ################# Loading & Layout ##########################
-df_mergedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/merged_data_state.csv")
-df_stackedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_state.csv")
-df_stackedDiag = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_diagnosis.csv")
-#st.dataframe(df_mergedState)
+@st.cache_data
+def load_data():
+   #df_mergedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/merged_data_state.csv")
+   df_stackedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_state.csv")
+   df_stackedDiag = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_diagnosis.csv")
+   return df_stackedState, df_stackedDiag
+df_stackedState, df_stackedDiag = load_data()
+
+#df_mergedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/merged_data_state.csv")
+#df_stackedState = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_state.csv")
+#df_stackedDiag = pd.read_csv("https://raw.githubusercontent.com/LinfengHu-1/bmi706_finalProject/main/stacked_data_diagnosis.csv")
+
+st.dataframe(df_stackedState.head())
 
 ### General Layout ###
 st.write("## Mental Health Outcomes and Intervention Investigation")
 tab1, tab2 = st.tabs(["Mental Health Outcomes", "Access to Psychiatric Care"])
-
 
 ################### Tab 1: Mental Health Outcomes #######################
 with tab1:
    task1 = st.header("Mental Health Outcomes: Temporal Trends")
    states = st.multiselect("States", options=df_stackedState["STATEFIP"].unique())
    chart = alt.Chart(df_stackedState).mark_line().encode(
-    x=alt.X("Year"),
-    y=alt.Y("MH1_values_1", title="MH1_values_1"),
-    #color=alt.Color("Rate", title="Mortality Rate (log scale)", scale=alt.Scale(type='log', domain=[0.01, 1000], clamp=True)),
-    #tooltip=["Rate"],
-).properties(
-    title=f"Mental Health Disorder rates in {states}",
-)
+      x=alt.X("Year"),
+      y=alt.Y("MH1_values_1", title="MH1_values_1"),
+      #color=alt.Color("Rate", title="Mortality Rate (log scale)", scale=alt.Scale(type='log', domain=[0.01, 1000], clamp=True)),
+      #tooltip=["Rate"],
+      ).properties(
+         title=f"Mental Health Disorder rates in {states}",
+         )
    st.altair_chart(chart, use_container_width=True)
 
 #year = st.slider("Year", min_value=df["Year"].min(), max_value=df["Year"].max())
